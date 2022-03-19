828c28479429ee70de26425c2226e44ae628cbde
huxiaoan <hu7006@outlook.com>
2022-03-19 20:56:01 +0800

    items生成函数

--- ESP32-IRremote-Gree/main/gree.c
+++ ESP32-IRremote-Gree/main/gree.c
@@ -2,24 +2,55 @@
 #include "stdlib.h"
 #include "esp_log.h"
 
-
 static const char *TAG = "GREE";
 
+int generate_gree_item(uint8_t cmd[], rmt_item32_t *item, uint8_t invert)
+{
+    if (item = calloc(70, sizeof(rmt_item32_t)))
+    {
+        //起始码
+        item[0].duration0=9000;
+        item[0].level0=!!invert;
+        item[0].duration1=4500;
+        item[0].level1=!invert;
 
+        for(int i=0;i<32;i++){
+            level_implement(cmd[i],&item[i+1],invert);
+        }
+        //连接码
+        item[33].duration0=600;
+        item[33].level0=!!invert;
+        item[33].duration1=20000;
+        item[33].level1=!invert;
 
-int generate_gree_item(uint8_t cmd[],rmt_item32_t* item,uint8_t invert){
-    if(item=calloc(35,sizeof(rmt_item32_t))){
+        for(int i=32;i<67;i++){
+            level_implement(cmd[i],&item[i+2],invert);
+        }
 
-
-
-    }else{
-        ESP_LOGE(TAG,"ERROR allocat memory for cmd items.");
+    }
+    else
+    {
+        ESP_LOGE(TAG, "ERROR allocat memory for cmd items.");
         ESP_ERROR_CHECK(ESP_FAIL);
     }
 }
 
-void level_implement(uint8_t level,rmt_item32_t* item,uint8_t invert){
-    if(0==level){
-        item->
+void level_implement(uint8_t level, rmt_item32_t *item, uint8_t invert)
+{
+    if (0 == level)
+    {
+        item->duration0=RMT_LOW_LEVEL_1;
+        item->level0=!!invert;
+        item->duration1=RMT_LOW_LEVEL_2;
+        item->level1=!invert;
+    }else if(1== level){
+        item->duration0=RMT_HIGH_LEVEL_1;
+        item->level0=!!invert;
+        item->duration1=RMT_HIGH_LEVEL_2;
+        item->level1=!invert;
     }
 }
+
+
+
+
