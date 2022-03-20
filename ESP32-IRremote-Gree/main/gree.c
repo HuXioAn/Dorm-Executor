a1e5b972f45cfa1609a58e1a65e95cb60d5ee6a8
huxiaoan <hu7006@outlook.com>
2022-03-20 15:35:25 +0800

    成功开机！

--- ESP32-IRremote-Gree/main/gree.c
+++ ESP32-IRremote-Gree/main/gree.c
@@ -7,7 +7,7 @@ static const char *TAG = "GREE";
 int generate_gree_item(uint8_t cmd[], rmt_item32_t **items, uint8_t invert)
 {   
     rmt_item32_t *item=NULL;
-    if ((item = calloc(70, sizeof(rmt_item32_t))))
+    if ((item = calloc(200, sizeof(rmt_item32_t))))
     {
         //起始码
         item[0].duration0=9000;
@@ -29,11 +29,65 @@ int generate_gree_item(uint8_t cmd[], rmt_item32_t **items, uint8_t invert)
         }
         item[69].duration0=600;
         item[69].level0=!!invert;
-        item[69].duration1=0;
+        item[69].duration1=20000;
         item[69].level1=!invert;
+        //40ms
+        item[70].duration0=10000;
+        item[70].level0=!invert;
+        item[70].duration1=10000;
+        item[70].level1=!invert;
+
+
+
+
+        //起始码
+        item[71].duration0=9000;
+        item[71].level0=!!invert;
+        item[71].duration1=4500;
+        item[71].level1=!invert;
+
+        for(int i=67;i<102;i++){
+            level_implement(cmd[i],&item[i+5],invert);
+        }
+        //连接码
+        item[107].duration0=600;
+        item[107].level0=!!invert;
+        item[107].duration1=20000;
+        item[107].level1=!invert;
+
+        for(int i=102;i<134;i++){
+            level_implement(cmd[i],&item[i+6],invert);
+        }
+        item[140].duration0=600;
+        item[140].level0=!!invert;
+        item[140].duration1=17000;
+        item[140].level1=!invert;
+        //77ms
+        item[141].duration0=30000;
+        item[141].level0=!invert;
+        item[141].duration1=30000;
+        item[141].level1=!invert;       
+        
+        for(int i=134;i<177;i++){
+            level_implement(cmd[i],&item[i+8],invert);
+        }
+
+        item[185].duration0=600;
+        item[185].level0=!!invert;
+        item[185].duration1=0;
+        item[185].level1=!invert; 
+
+
+
+
+
+
+
+
+
 
         *items=item;
-        return 70;
+        return 186;
     }
     else
     {
--- ESP32-IRremote-Gree/main/main.c
+++ ESP32-IRremote-Gree/main/main.c
@@ -14,23 +14,56 @@ static const char *TAG = "ESP32-IR-GREE";
 
 void app_main(void)
 {
-    uint8_t cmd[67] = {1,0,0,0,0,0,1,
+    uint8_t cmd[177] = {    1,0,0,1,0,0,0,0,1,
+                            0,0,1,0,0,0,0,0,0,
+                            0,0,0,1,0,0,0,0,0,0,
+							1,0,1,0,0,1,0,
+                            //20ms
+                            0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,
+                            0,0,0,0,0,0,0,0,0,
+                            0,0,0,1,1,
+                            //40ms
+                            1,0,0,1,0,0,0,0,1,
+                            0,0,1,0,0,0,0,0,0,
+                            0,0,0,1,0,0,0,0,0,0,
+							1,1,1,0,0,1,0,
+                            //20ms
+                            0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,
+                            0,0,0,0,0,0,0,0,0,
+                            0,0,0,1,1,
+                            //77ms
+                            1,0,0,1,0,0,0,0,1,
+                            0,0,1,0,0,0,0,0,0,
+                            0,0,0,1,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,
+                            1,0,1,0,0,1,0
+                            
+                            
+                            
+                            
+                            };
+
+    uint8_t cmdo[67] = {1,0,0,0,0,0,1,
 							0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,
 							1,0,1,0,0,1,0,1,0,0,0,0,0,0,0,0,
 							0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
 							0,1,0}; 
 
     rmt_item32_t* items=NULL;
-    int length = generate_gree_item(cmd,&items,0);
+    int length = generate_gree_item(cmd,&items,1);
     ESP_LOGI(TAG,"items generated.\n");
     rmt_config_t rmt_tx_config = RMT_DEFAULT_CONFIG_TX(RMT_TX_GPIO,RMT_CHANNEL_0);
     rmt_tx_config.tx_config.carrier_en = true;
+    //rmt_tx_config.tx_config.carrier_level = RMT_CARRIER_LEVEL_LOW;
+    //rmt_tx_config.tx_config.idle_level = RMT_IDLE_LEVEL_LOW;
     rmt_config(&rmt_tx_config);
     rmt_driver_install(RMT_CHANNEL_0, 0, 0);
 
-
+    vTaskDelay(1000/portTICK_PERIOD_MS);
     rmt_write_items(RMT_CHANNEL_0,items,length,true);
-
+    vTaskDelay(1000/portTICK_PERIOD_MS);
     free(items);
     rmt_driver_uninstall(RMT_CHANNEL_0);
     ESP_LOGI(TAG,"RMT DRIVER UNINSTALLED.\n");
